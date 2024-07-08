ff08f81526026a11736558c13bb9969fd0449685
zhuxiaodong <zhuxiaodong@eenet.com>
2024-07-08 18:38:45 +0800

    测试文件

---
+++ main/FirstG.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/first", func(context *gin.Context) {
+		fmt.Sprintf("111ss1 %s", "sfsf")
+		name := context.DefaultQuery("name", "don")
+		job := context.Query("job")
+
+		//context.String(http.StatusOK, "first name: %s , job: %s", name, job)
+
+		router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+			// your custom format
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+			)
+		}))
+
+		context.JSON(http.StatusOK, gin.H{
+			"status":  "posted",
+			"message": "ok",
+			"job":     job,
+			"name":    name,
+		})
+	})
+
+	router.GET("/second", func(context *gin.Context) {
+		name := context.DefaultQuery("name", "sss")
+		job := context.Query("job")
+
+		//context.String(http.StatusOK, "first name: %s , job: %s", name, job)
+
+		router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+			// your custom format
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+			)
+		}))
+
+		context.JSON(http.StatusOK, gin.H{
+			"status":  "posted",
+			"message": "ok",
+			"job":     job,
+			"name":    name,
+		})
+	})
+
+	router.POST("/loginJSON", func(context *gin.Context) {
+		var json Login
+		router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+			// your custom format
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"    %s\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+				json,
+			)
+		}))
+
+		if err := context.ShouldBindJSON(&json); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "don" || json.Password != "123" {
+			context.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	router.Use(gin.Recovery())
+	router.Run(":8030")
+}
